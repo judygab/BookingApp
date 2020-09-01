1077711c504c2e70b4656646ac3979fb0fc7a1d4
judygab <ivditigabeskiria@gmail.com>
2020-08-31 23:38:41 -0400

    added redux

---
+++ src/redux/CartItems.js
@@ -0,0 +1,13 @@
+const initalState = []
+
+const cartItemsReducer - (state = initialState, action) => {
+  switch(action.type) {
+    case ADD_TO_CART:
+      return [...state, action.payload]
+    case REMOVE_FROM_CART:
+      return state.filter(cartItem => cartItem.id !== action.payload.id)
+  }
+  return state
+}
+
+export default cartItemsReducer
